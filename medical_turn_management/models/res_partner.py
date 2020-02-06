45f67b6abdacf2c28bd94ec287e1d810aac8fbff
Enric Tobella <etobella@creublanca.es>
2020-02-06 13:32:05 +0100

    [11.0][ADD] medical_turn_management

---
+++ medical_turn_management/models/res_partner.py
@@ -0,0 +1,12 @@
+# Copyright 2020 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    turn_specialty_ids = fields.Many2many(
+        'medical.turn.specialty',
+    )
