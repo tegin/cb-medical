cf0a964f3d94234655726539d84852489057c00d
Enric Tobella <etobella@creublanca.es>
2020-02-06 13:32:05 +0100

    [11.0][ADD] medical_turn_management

---
+++ medical_turn_management/__manifest__.py
@@ -0,0 +1,29 @@
+# Copyright 2020 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Medical Turn Management',
+    'summary': """
+        Manage Profesional turn management""",
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Creu Blanca,Odoo Community Association (OCA)',
+    'website': 'www.creublanca.es',
+    'depends': [
+        'medical_administration_practitioner',
+        'web_view_calendar_list',
+        'medical_clinical',
+        'cb_medical_administration_center',
+    ],
+    'data': [
+        'security/medical_security.xml',
+        'security/ir.model.access.csv',
+        'views/medical_menu.xml',
+        'wizards/wzd_medical_turn.xml',
+        'views/res_partner.xml',
+        'views/medical_turn_specialty.xml',
+        'views/medical_turn.xml',
+    ],
+    'demo': [
+    ],
+}
