1868537a51cc99d478645e8d7aa4ac10598dfa32
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-02-19 14:49:22 +0100

    [12.0][MIG] cb_medical_careplan_sale

--- cb_medical_careplan_sale/models/res_config_settings.py
+++ cb_medical_careplan_sale/models/res_config_settings.py
@@ -6,9 +6,10 @@ from odoo import api, fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    default_third_party_product = fields.Many2one(
+    def_third_party_product = fields.Many2one(
         "product.product",
         "Third party Product",
+        oldname="default_third_party_product",
         domain="[('type', '=', 'service')]",
         help="Default product used for third party sale orders",
     )
@@ -16,26 +17,26 @@ class ResConfigSettings(models.TransientModel):
     @api.model
     def get_values(self):
         res = super(ResConfigSettings, self).get_values()
-        default_third_party_product = literal_eval(
+        def_third_party_product = literal_eval(
             self.env["ir.config_parameter"]
             .sudo()
-            .get_param("cb.default_third_party_product", default="False")
+            .get_param("cb.def_third_party_product", default="False")
         )
         if (
-            default_third_party_product
+            def_third_party_product
             and not self.env["product.product"]
-            .browse(default_third_party_product)
+            .browse(def_third_party_product)
             .exists()
         ):
-            default_third_party_product = False
-        res.update(default_third_party_product=default_third_party_product)
+            def_third_party_product = False
+        res.update(def_third_party_product=def_third_party_product)
         return res
 
     @api.multi
     def set_values(self):
         res = super(ResConfigSettings, self).set_values()
         self.env["ir.config_parameter"].sudo().set_param(
-            "cb.default_third_party_product",
-            self.default_third_party_product.id,
+            "cb.def_third_party_product",
+            self.def_third_party_product.id,
         )
         return res
