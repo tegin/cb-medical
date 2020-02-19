db287304bc07f0b80e315eabfe9f6a5cd723aef8
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-02-19 13:19:04 +0100

    [12.0][MIG] cb_medical_workflow_trigger

--- cb_medical_workflow_trigger/__manifest__.py
+++ cb_medical_workflow_trigger/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Medical Workflow Triggers",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Eficent, Creu Blanca",
     "depends": ["cb_medical_workflow_plandefinition"],
     "data": [
