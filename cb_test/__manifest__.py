773b35e265007c0c6bd867404a146b28a047d3f5
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-02-19 15:08:07 +0100

    [12.0][MIG] cb_test

--- cb_test/__manifest__.py
+++ cb_test/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "CB Testing",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Eficent, Creu Blanca",
     "depends": [
         "medical_document_zpl2",
@@ -12,7 +12,6 @@
         "cb_medical_state_update",
         "cb_medical_invoice",
     ],
-    "data": [],
     "website": "https://github.com/OCA/vertical-medical",
     "license": "LGPL-3",
     "installable": True,
