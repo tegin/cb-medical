f8b2e569bc4fa1b4256eca3b95288b44e0d86d1c
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-02-20 13:23:34 +0100

    [12.0][MIG] cb_medical_administration_healthcare_service

--- cb_medical_administration_healthcare_service/__manifest__.py
+++ cb_medical_administration_healthcare_service/__manifest__.py
@@ -4,14 +4,13 @@
 
 {
     "name": "Medical Patient Healthcare Service CB",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Eficent, Creu Blanca",
     "depends": [
         "medical_administration_healthcare_service",
         "cb_medical_administration_practitioner_specialty",
     ],
     "data": ["views/res_partner_views.xml"],
-    "demo": [],
     "website": "https://github.com/OCA/cb-addons",
     "license": "LGPL-3",
     "installable": True,
