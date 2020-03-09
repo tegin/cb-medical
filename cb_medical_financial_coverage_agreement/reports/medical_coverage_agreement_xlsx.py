2d9a5022294293a9ab3de1b7496057fdd6941ded
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-03-09 16:53:07 +0100

    [12.0][MIG] medical_turn_management

--- cb_medical_financial_coverage_agreement/reports/medical_coverage_agreement_xlsx.py
+++ cb_medical_financial_coverage_agreement/reports/medical_coverage_agreement_xlsx.py
@@ -48,7 +48,7 @@ class MedicalCoverageAgreementXlsx(models.AbstractModel):
         for agreement in agreements:
             report_name = agreement.name
             sheet = workbook.add_worksheet(report_name[:31])
-            items = agreement._agreement_report_data(private)
+            items = agreement._agreement_report_data(print_coverage=private)
             i = 0
             if items:
                 th = max([self.tree_height(item) for item in items])
