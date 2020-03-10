9fe26bb3e28dcb76f7d24adf8e449d67aca85969
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-03-10 18:29:18 +0100

    [12.0] black

--- cb_nonconformity_encounter/tests/test_nonconformity_encounter.py
+++ cb_nonconformity_encounter/tests/test_nonconformity_encounter.py
@@ -5,31 +5,36 @@ from odoo.tests.common import TransactionCase
 
 
 class TestNonconformityEncounter(TransactionCase):
-
     def setUp(self):
         super(TestNonconformityEncounter, self).setUp()
-        self.origin = self.env['mgmtsystem.nonconformity.origin'].create({
-            'name': 'origin',
-            'from_encounter': True,
-            'responsible_user_id': self.env.uid,
-            'manager_user_id': self.env.uid,
-        })
-        self.patient = self.env['medical.patient'].create({'name': 'Patient'})
-        self.encounter_id = self.env['medical.encounter'].create({
-            'patient_id': self.patient.id
-        })
+        self.origin = self.env["mgmtsystem.nonconformity.origin"].create(
+            {
+                "name": "origin",
+                "from_encounter": True,
+                "responsible_user_id": self.env.uid,
+                "manager_user_id": self.env.uid,
+            }
+        )
+        self.patient = self.env["medical.patient"].create({"name": "Patient"})
+        self.encounter_id = self.env["medical.encounter"].create(
+            {"patient_id": self.patient.id}
+        )
 
     def test_nonconformity_encounter(self):
-        wizard = self.env['wizard.create.nonconformity'].with_context(
-            active_id=self.encounter_id.id
-        ).create({
-            'name': 'Title',
-            'description': 'Description',
-            'origin_id': self.origin.id,
-        })
+        wizard = (
+            self.env["wizard.create.nonconformity"]
+            .with_context(active_id=self.encounter_id.id)
+            .create(
+                {
+                    "name": "Title",
+                    "description": "Description",
+                    "origin_id": self.origin.id,
+                }
+            )
+        )
         wizard.create_quality_issue()
-        issue = self.env['mgmtsystem.quality.issue'].search(
-            [('encounter_id', '=', self.encounter_id.id)]
+        issue = self.env["mgmtsystem.quality.issue"].search(
+            [("encounter_id", "=", self.encounter_id.id)]
         )
         self.assertTrue(issue)
         issue.to_nonconformity()
@@ -37,4 +42,4 @@ class TestNonconformityEncounter(TransactionCase):
             issue.non_conformity_id.encounter_id.id, self.encounter_id.id
         )
         action = self.encounter_id.action_view_quality_issues()
-        self.assertEqual(action['res_id'], issue.id)
+        self.assertEqual(action["res_id"], issue.id)
