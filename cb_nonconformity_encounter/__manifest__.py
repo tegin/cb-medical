e4f86bf7a9b3776fa385e97e97cd7fd7392b6cd9
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-03-10 18:29:18 +0100

    [12.0] black

--- cb_nonconformity_encounter/__manifest__.py
+++ cb_nonconformity_encounter/__manifest__.py
@@ -2,23 +2,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Cb Nonconformity Encounter',
-    'summary': """
+    "name": "Cb Nonconformity Encounter",
+    "summary": """
         CB custom nonconformity management""",
-    'version': '11.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Creu Blanca',
-    'website': 'www.creublanca.es',
-    'depends': [
-        'cb_mgmtsystem_issue',
-        'medical_administration_encounter',
-    ],
-    'data': [
-        'views/mgmtsystem_quality_issue.xml',
-        'data/nonconformity_sequence_data.xml',
-        'views/mgmtsystem_nonconformity_origin.xml',
-        'wizards/wizard_create_nonconformity.xml',
-        'views/medical_encounter.xml',
-        'views/mgmtsystem_nonconformity.xml',
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca",
+    "website": "www.creublanca.es",
+    "depends": ["cb_mgmtsystem_issue", "medical_administration_encounter"],
+    "data": [
+        "views/mgmtsystem_quality_issue.xml",
+        "data/nonconformity_sequence_data.xml",
+        "views/mgmtsystem_nonconformity_origin.xml",
+        "wizards/wizard_create_nonconformity.xml",
+        "views/medical_encounter.xml",
+        "views/mgmtsystem_nonconformity.xml",
     ],
 }
--- cb_nonconformity_encounter/models/mgmtsystem_nonconformity_origin.py
+++ cb_nonconformity_encounter/models/mgmtsystem_nonconformity_origin.py
@@ -6,11 +6,11 @@ from odoo import fields, models
 
 class MgmtsystemNonconformityOrigin(models.Model):
 
-    _inherit = 'mgmtsystem.nonconformity.origin'
+    _inherit = "mgmtsystem.nonconformity.origin"
 
     from_encounter = fields.Boolean()
 
     responsible_user_id = fields.Many2one(
-        'res.users', string="Default Responsible",
+        "res.users", string="Default Responsible"
     )
-    manager_user_id = fields.Many2one('res.users', string="Default Manager")
+    manager_user_id = fields.Many2one("res.users", string="Default Manager")
