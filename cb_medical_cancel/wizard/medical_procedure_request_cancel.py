f961e87e1cf6d9256cc285666d3fe8762362fc1c
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-03-10 18:29:18 +0100

    [12.0] black

--- cb_medical_cancel/wizard/medical_procedure_request_cancel.py
+++ cb_medical_cancel/wizard/medical_procedure_request_cancel.py
@@ -4,6 +4,6 @@ from odoo import fields, models
 class MedicalProcedureRequestCancel(models.TransientModel):
     _name = "medical.procedure.request.cancel"
     _inherit = "medical.request.cancel"
-    _description = 'medical.procedure.request.cancel'
+    _description = "medical.procedure.request.cancel"
 
     request_id = fields.Many2one("medical.procedure.request")
