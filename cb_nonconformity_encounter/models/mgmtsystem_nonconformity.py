a5803d6d1742443d62136ddc0e8dde7792e31e9d
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-03-10 18:29:18 +0100

    [12.0] black

--- cb_nonconformity_encounter/models/mgmtsystem_nonconformity.py
+++ cb_nonconformity_encounter/models/mgmtsystem_nonconformity.py
@@ -6,17 +6,17 @@ from odoo import api, fields, models
 
 class MgmtsystemNonconformity(models.Model):
 
-    _inherit = 'mgmtsystem.nonconformity'
+    _inherit = "mgmtsystem.nonconformity"
 
-    encounter_id = fields.Many2one('medical.encounter')
+    encounter_id = fields.Many2one("medical.encounter")
 
-    ref = fields.Char(readonly=True, copy=False, default='/')
+    ref = fields.Char(readonly=True, copy=False, default="/")
 
     @api.model
     def create(self, vals):
-        if vals.get('ref', '/') == '/':
+        if vals.get("ref", "/") == "/":
             sequence = self.env.ref(
-                'mgmtsystem_nonconformity.seq_mgmtsystem_nonconformity'
+                "mgmtsystem_nonconformity.seq_mgmtsystem_nonconformity"
             )
-            vals['ref'] = sequence.next_by_id()
+            vals["ref"] = sequence.next_by_id()
         return super().create(vals)
