8d0bf73ef1fab77a72bdd7ac9f953bd9bae35939
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-03-10 18:29:18 +0100

    [12.0] black

--- cb_nonconformity_encounter/models/medical_encounter.py
+++ cb_nonconformity_encounter/models/medical_encounter.py
@@ -6,17 +6,17 @@ from odoo import api, fields, models
 
 class MedicalEncounter(models.Model):
 
-    _inherit = 'medical.encounter'
+    _inherit = "medical.encounter"
 
     quality_issue_ids = fields.One2many(
-        'mgmtsystem.quality.issue', inverse_name='encounter_id'
+        "mgmtsystem.quality.issue", inverse_name="encounter_id"
     )
 
     quality_issue_count = fields.Integer(
-        compute='_compute_quality_issue_count', store=True
+        compute="_compute_quality_issue_count", store=True
     )
 
-    @api.depends('quality_issue_ids')
+    @api.depends("quality_issue_ids")
     def _compute_quality_issue_count(self):
         for record in self:
             record.quality_issue_count = len(record.quality_issue_ids)
@@ -24,14 +24,18 @@ class MedicalEncounter(models.Model):
     @api.multi
     def action_view_quality_issues(self):
         action = self.env.ref(
-            'cb_mgmtsystem_issue.mgmtsystem_quality_issue_act_window'
+            "cb_mgmtsystem_issue.mgmtsystem_quality_issue_act_window"
         ).read()[0]
         if len(self.quality_issue_ids) > 1:
-            action['domain'] = [('id', 'in', self.quality_issue_ids.ids)]
+            action["domain"] = [("id", "in", self.quality_issue_ids.ids)]
         elif self.quality_issue_ids:
-            action['views'] = [
-                (self.env.ref(
-                    'cb_mgmtsystem_issue.mgmtsystem_quality_issue_form_view'
-                ).id, 'form')]
-            action['res_id'] = self.quality_issue_ids.id
+            action["views"] = [
+                (
+                    self.env.ref(
+                        "cb_mgmtsystem_issue.mgmtsystem_quality_issue_form_view"
+                    ).id,
+                    "form",
+                )
+            ]
+            action["res_id"] = self.quality_issue_ids.id
         return action
