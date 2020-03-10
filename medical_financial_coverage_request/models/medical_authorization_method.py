8eac3323cdf1199db270dbb1381463be4e667382
Enric Tobella <etobella@creublanca.es>
2020-03-10 15:55:17 +0100

    [12.0][IMP] coverage_request: Add web model

--- medical_financial_coverage_request/models/medical_authorization_method.py
+++ medical_financial_coverage_request/models/medical_authorization_method.py
@@ -2,7 +2,7 @@
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import models, fields
+from odoo import api, fields, models
 
 
 class MedicalAuthorizationMethod(models.Model):
@@ -39,3 +39,26 @@ class MedicalAuthorizationMethod(models.Model):
         track_visibility=True,
     )
     integration_information = fields.Char()
+    authorization_web_id = fields.Many2one(
+        'medical.authorization.web',
+    )
+
+    def _update_web_method(self, vals):
+        authorization_web_id = vals.get('authorization_web_id', False)
+        if authorization_web_id:
+            authorization_web = self.env['medical.authorization.web'].browse(
+                authorization_web_id
+            )
+            vals.update({
+                'integration_information': authorization_web.link,
+                'method_information': authorization_web.notes,
+            })
+            return vals
+        return vals
+
+    @api.model
+    def create(self, vals):
+        return super().create(self._update_web_method(vals))
+
+    def write(self, vals):
+        return super().write(self._update_web_method(vals))
