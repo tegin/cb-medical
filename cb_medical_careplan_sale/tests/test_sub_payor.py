304c9a91cfd247e38688d954f6a3fbcfe30df456
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-05-11 09:52:52 +0200

    [12.0][FIX] remove edit_subpayor

--- cb_medical_careplan_sale/tests/test_sub_payor.py
+++ cb_medical_careplan_sale/tests/test_sub_payor.py
@@ -13,16 +13,6 @@ class TestMedicalSubPayor(TransactionCase):
             {"name": "Payor", "is_payor": True}
         )
 
-    def test_sub_payor(self):
-        sub_payor = self.env["res.partner"].create(
-            {
-                "name": "Sub Payor",
-                "is_sub_payor": True,
-                "payor_id": self.payor.id,
-            }
-        )
-        self.assertTrue(sub_payor.edit_sub_payor)
-
     def test_constrain(self):
         with self.assertRaises(ValidationError):
             self.env["res.partner"].create(
