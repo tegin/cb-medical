eaca77875a6a43cc81e197b79d15320896595637
Enric Tobella <etobella@creublanca.es>
2020-05-19 12:38:17 +0200

    [IMP][12.0] sale_invoice_group_method: Define on line

--- cb_medical_sale_invoice_group_method/models/medical_coverage_agreement_item.py
+++ cb_medical_sale_invoice_group_method/models/medical_coverage_agreement_item.py
@@ -2,14 +2,21 @@
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import models
+from odoo import fields, models
 
 
 class MedicalCoverageAgreementItem(models.Model):
     _inherit = "medical.coverage.agreement.item"
 
+    invoice_group_method_id = fields.Many2one("invoice.group.method")
+
     def _check_authorization(self, method, **kwargs):
         vals = super()._check_authorization(method, **kwargs)
+        if (
+            method.force_item_authorization_method
+            and self.invoice_group_method_id
+        ):
+            vals["invoice_group_method_id"] = self.invoice_group_method_id.id
         if "invoice_group_method_id" not in vals:
             vals["invoice_group_method_id"] = (
                 method.invoice_group_method_id.id
