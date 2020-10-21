44d54ab6ab89481f1dd2cc04021da817991192e1
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-10-21 12:17:12 +0200

    [12.0][FIX] mgmnt issues: correct subscriptions

--- cb_nonconformity_encounter/__manifest__.py
+++ cb_nonconformity_encounter/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Cb Nonconformity Encounter",
     "summary": """
         CB custom nonconformity management""",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca",
     "website": "www.creublanca.es",
