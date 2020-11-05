2eb7eeadc112bac2ad10c921ad7374a6dfc8605e
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-11-05 09:37:48 +0100

    [FIX] pre-commit

--- cb_medical_administration_requester/models/res_partner.py
+++ cb_medical_administration_requester/models/res_partner.py
@@ -1,6 +1,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models, fields
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
