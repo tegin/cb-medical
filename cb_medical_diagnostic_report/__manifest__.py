2763d0c4afde7b21b8f11d06fe13c85b5dd5dc94
Alba Riera <alba.riera@creublanca.es>
2021-03-24 13:19:43 +0100

    [ADD] cb_medical_diagnostic_report
    Add report category
    
    + print templates
    
    Remove administration department

---
+++ cb_medical_diagnostic_report/__manifest__.py
@@ -0,0 +1,27 @@
+# Copyright 2021 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Cb Medical Diagnostic Report",
+    "summary": """Allows the creation of medical diagnostic reports""",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca",
+    "website": "https://github.com/tegin/medical-fhir",
+    "depends": [
+        "medical_diagnostic_report",
+        "medical_administration",
+        "medical_signature_storage",
+    ],
+    "data": [
+        "security/department_security.xml",
+        "security/ir.model.access.csv",
+        "data/ir_sequence_data.xml",
+        "views/medical_department.xml",
+        "views/medical_diagnostic_report_template.xml",
+        "views/medical_diagnostic_report.xml",
+        "views/medical_report_category.xml",
+        "reports/medical_diagnostic_report_template.xml",
+        "wizards/medical_diagnostic_report_expand.xml",
+    ],
+}
