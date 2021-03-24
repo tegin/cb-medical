bd0c080428bfeb0241402d52490af4524af96079
Alba Riera <alba.riera@creublanca.es>
2021-03-24 13:19:43 +0100

    [ADD] cb_medical_diagnostic_report
    Add report category
    
    + print templates
    
    Remove administration department

---
+++ cb_medical_diagnostic_report/models/medical_department.py
@@ -0,0 +1,23 @@
+# Copyright 2021 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class MedicalDepartment(models.Model):
+
+    _name = "medical.department"
+    _inherit = "medical.abstract"
+    _description = "Medical Department"
+
+    name = fields.Char(required=True)
+    diagnostic_report_header = fields.Html(translate=True, sanitize=False)
+    report_category_ids = fields.One2many(
+        comodel_name="medical.report.category",
+        inverse_name="medical_department_id",
+    )
+
+    def _get_internal_identifier(self, vals):
+        return (
+            self.env["ir.sequence"].next_by_code("medical.department") or "/"
+        )
