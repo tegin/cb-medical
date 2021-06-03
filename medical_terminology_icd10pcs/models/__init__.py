60772fd26b8308e43a601d7156c926e08ffb24d0
Enric Tobella <etobella@creublanca.es>
2021-06-03 15:00:23 +0200

    [IMP] githubactions: Copy logic from OCA

--- medical_terminology_icd10pcs/models/__init__.py
+++ medical_terminology_icd10pcs/models/__init__.py
@@ -1,6 +1,6 @@
 # Copyright 2017 Creu Blanca
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from . import medical_icd10pcs_section
 from . import medical_icd10pcs_body_system
