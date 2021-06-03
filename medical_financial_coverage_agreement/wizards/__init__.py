d9cafc720dcfe5fb79a5df90a9e70a76378edfe2
Enric Tobella <etobella@creublanca.es>
2021-06-03 15:00:23 +0200

    [IMP] githubactions: Copy logic from OCA

--- medical_financial_coverage_agreement/wizards/__init__.py
+++ medical_financial_coverage_agreement/wizards/__init__.py
@@ -1,6 +1,6 @@
 # Copyright 2017 Creu Blanca
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from . import medical_agreement_change_prices
 from . import medical_coverage_agreement_template
