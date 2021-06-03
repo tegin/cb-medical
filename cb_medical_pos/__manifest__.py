6941ec90fb83e4488c7d2d9a0bc931d2be02d027
Enric Tobella <etobella@creublanca.es>
2021-06-03 15:00:23 +0200

    [IMP] githubactions: Copy logic from OCA

--- cb_medical_pos/__manifest__.py
+++ cb_medical_pos/__manifest__.py
@@ -1,6 +1,6 @@
 # Copyright 2017 Creu Blanca
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "CB Medical link to PoS",
@@ -26,7 +26,7 @@
         "views/report_invoice.xml",
     ],
     "website": "https://github.com/OCA/cb-addons",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "installable": True,
     "auto_install": False,
 }
