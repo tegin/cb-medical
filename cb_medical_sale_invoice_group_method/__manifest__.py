e3c86e0fcd1bc3d0db8dce6c7593672bc96be317
Enric Tobella <etobella@creublanca.es>
2021-06-03 15:00:23 +0200

    [IMP] githubactions: Copy logic from OCA

--- cb_medical_sale_invoice_group_method/__manifest__.py
+++ cb_medical_sale_invoice_group_method/__manifest__.py
@@ -1,6 +1,6 @@
 # Copyright 2017 Creu Blanca
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Medical Sale Invoice Group Method",
@@ -33,7 +33,7 @@
         "report/medical_preinvoice_group.xml",
     ],
     "website": "https://github.com/OCA/vertical-medical",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "installable": True,
     "auto_install": False,
 }
