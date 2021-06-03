fad48fa22b5ff4d06e20a3eb5ee6c63b44fb66d8
Enric Tobella <etobella@creublanca.es>
2021-06-03 15:00:23 +0200

    [IMP] githubactions: Copy logic from OCA

--- cb_medical_workflow_activity/__manifest__.py
+++ cb_medical_workflow_activity/__manifest__.py
@@ -1,6 +1,6 @@
 # Copyright 2017 Creu Blanca
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "CB Medical Workflow Activity",
@@ -9,7 +9,7 @@
     "depends": ["medical_clinical_procedure"],
     "data": ["views/product_views.xml"],
     "website": "https://github.com/OCA/vertical-medical",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "installable": True,
     "auto_install": False,
 }
