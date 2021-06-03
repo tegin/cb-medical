fb8d66f80ff8cea852f9288fb9c326ac2614c657
Enric Tobella <etobella@creublanca.es>
2021-06-03 15:00:23 +0200

    [IMP] githubactions: Copy logic from OCA

--- cb_medical_invoice/__manifest__.py
+++ cb_medical_invoice/__manifest__.py
@@ -1,6 +1,6 @@
 # Copyright 2017 Creu Blanca
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "CB Medical sequence configuration",
@@ -16,7 +16,7 @@
         "views/res_partner_views.xml",
     ],
     "website": "https://github.com/OCA/vertical-medical",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "installable": True,
     "auto_install": False,
 }
