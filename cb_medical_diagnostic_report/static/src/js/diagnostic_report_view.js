fba2f627fcec312ab9592366e5003a2f6a315401
Enric Tobella <etobella@creublanca.es>
2021-06-10 15:02:03 +0200

    [IMP] cb_medical_diagnostic_report: Allow to use images as a list

---
+++ cb_medical_diagnostic_report/static/src/js/diagnostic_report_view.js
@@ -0,0 +1,22 @@
+odoo.define("diagnostic_report.DiagnosticReportView", function(require) {
+    "use strict";
+
+    var registry = require("web.view_registry");
+
+    var FormView = require("web.FormView");
+
+    var DiagnosticReportController = require("diagnostic_report.DiagnosticReportController");
+
+    var DiagnosticReportRenderer = require("diagnostic_report.DiagnosticReportRenderer");
+
+    var DiagnosticReportView = FormView.extend({
+        config: _.extend({}, FormView.prototype.config, {
+            Controller: DiagnosticReportController,
+            Renderer: DiagnosticReportRenderer,
+        }),
+    });
+
+    registry.add("diagnostic_report", DiagnosticReportView);
+
+    return DiagnosticReportView;
+});
