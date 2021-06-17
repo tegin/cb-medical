ce568c1c6d4ce5d3e970cfb77567dfd7ca117f26
Enric Tobella <etobella@creublanca.es>
2021-06-17 11:32:31 +0200

    [FIX] cb_medical_diagnostic_report: Paste moved to all...

--- cb_medical_diagnostic_report/static/src/js/diagnostic_report_renderer.js
+++ cb_medical_diagnostic_report/static/src/js/diagnostic_report_renderer.js
@@ -3,7 +3,19 @@ odoo.define("diagnostic_report.DiagnosticReportRenderer", function(require) {
 
     var FormRenderer = require("web.FormRenderer");
 
-    var DiagnosticReportRenderer = FormRenderer.extend({});
+    var DiagnosticReportRenderer = FormRenderer.extend({
+        events: _.extend({}, FormRenderer.prototype.events, {
+            paste: "_onPaste",
+        }),
+        _onPaste: function(e) {
+            if (this.mode !== "readonly") {
+                return;
+            }
+            this.trigger_up("paste_file", {
+                clipboardData: e.originalEvent.clipboardData,
+            });
+        },
+    });
 
     return DiagnosticReportRenderer;
 });
