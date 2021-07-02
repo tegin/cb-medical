340d965830050b8460bb8f0ff99ad6be3ed156bc
Olga Marco <olga.marco@creublanca.es>
2021-07-02 09:27:27 +0200

    [MIG] medical_signature_storage: Migration to 13.0

--- medical_signature_storage/__manifest__.py
+++ medical_signature_storage/__manifest__.py
@@ -5,12 +5,11 @@
     "name": "Medical Signature Storage",
     "summary": """
         Store User signature""",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "creublanca.es",
     "depends": [
-        "web_widget_digitized_signature",
         # 'medical_diagnostic_report',
     ],
     "data": [
