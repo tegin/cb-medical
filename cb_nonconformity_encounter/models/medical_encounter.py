e0eb3aa3c2acef89e6079768ab623bb3cb4a497c
Olga Marco <olga.marco@creublanca.es>
2021-07-02 09:15:31 +0200

    [MIG] cb_nonconformity_encounter: Migration to 13.0

--- cb_nonconformity_encounter/models/medical_encounter.py
+++ cb_nonconformity_encounter/models/medical_encounter.py
@@ -7,6 +7,3 @@ from odoo import models
 class MedicalEncounter(models.Model):
     _name = "medical.encounter"
     _inherit = ["medical.encounter", "mgmtsystem.quality.issue.abstract"]
-
-    def _get_quality_issue_partner(self):
-        return self.patient_id.partner_id
