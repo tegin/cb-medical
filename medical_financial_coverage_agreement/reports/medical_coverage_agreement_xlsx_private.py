94c80095eaee094f33c094b6cd4208555529544f
Olga Marco <olga.marco@creublanca.es>
2021-07-05 11:47:35 +0200

    [MIG] cb_medical_financial_coverage_agreement: Migration to 13.0

--- medical_financial_coverage_agreement/reports/medical_coverage_agreement_xlsx_private.py
+++ medical_financial_coverage_agreement/reports/medical_coverage_agreement_xlsx_private.py
@@ -2,5 +2,6 @@ from odoo import models
 
 
 class MedicalCoverageAgreementXlsx(models.AbstractModel):
-    _name = "report.cb_medical_financial_coverage_agreement.mca_xlsx_private"
-    _inherit = "report.cb_medical_financial_coverage_agreement.mca_xlsx"
+    _name = "report.medical_financial_coverage_agreement.mca_xlsx_private"
+    _inherit = "report.medical_financial_coverage_agreement.mca_xlsx"
+    _description = "Report CB Medical Financial Coverage Agreement Private"
