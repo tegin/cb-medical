0fa94b309f3ed2a838684a65afd764a4eefd4761
Alba Riera <alba.riera@creublanca.es>
2021-07-06 16:12:14 +0200

    [MIG] cb_medical_financial_coverage_request: Migration to 13.0

--- medical_financial_coverage_request/models/workflow_plan_definition.py
+++ medical_financial_coverage_request/models/workflow_plan_definition.py
@@ -8,8 +8,8 @@ from odoo import models
 class PlanDefinition(models.Model):
     _inherit = "workflow.plan.definition"
 
-    def get_request_group_vals(self, vals):
-        res = super().get_request_group_vals(vals)
+    def _get_request_group_vals(self, vals):
+        res = super()._get_request_group_vals(vals)
         res["parent_model"] = self.env.context.get("origin_model", False)
         res["parent_id"] = self.env.context.get("origin_id", False)
         if not res.get("is_billable", False):
