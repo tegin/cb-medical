1ca5a76222ad0b1f1902442f99a010306f0b93f9
Alba Riera <alba.riera@creublanca.es>
2021-07-08 18:00:17 +0200

    [MIG] crm_agreeement: Migration to 13.0

--- crm_agreement/__manifest__.py
+++ crm_agreement/__manifest__.py
@@ -5,14 +5,14 @@
     "name": "Crm Agreement",
     "summary": """
         Link of Medical Agreements and CRM""",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "www.creublanca.es",
     "depends": [
         "sale_crm",
         "cb_medical_quote",
-        "cb_medical_financial_coverage_agreement",
+        "medical_financial_coverage_agreement",
         "cb_medical_administration_requester",
     ],
     "data": [
