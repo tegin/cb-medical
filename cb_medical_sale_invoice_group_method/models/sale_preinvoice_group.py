7c24714013bac95705f48390f891034044c81c71
Enric Tobella <etobella@creublanca.es>
2021-07-08 17:40:52 +0200

    [FIX] cb_medical_sale_invoice_group_method: Removing unnecessary function

--- cb_medical_sale_invoice_group_method/models/sale_preinvoice_group.py
+++ cb_medical_sale_invoice_group_method/models/sale_preinvoice_group.py
@@ -125,7 +125,7 @@ class SalePreinvoiceGroup(models.Model):
         ]
 
     def create_invoice_values(self):
-        inv_data = self.validated_line_ids[0]._prepare_invoice()
+        inv_data = self.validated_line_ids[0].order_id._prepare_invoice()
         inv_data["agreement_id"] = self.agreement_id.id or False
         inv_data["name"] = self.internal_identifier
         journal = self.invoice_group_method_id.get_journal(self.company_id)
