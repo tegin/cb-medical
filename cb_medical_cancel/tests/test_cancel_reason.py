bfd07337549189ca9c78c5518ae4cb55f0f337b4
Alba Riera <alba.riera@creublanca.es>
2021-07-08 09:26:59 +0200

    [MIG] cb_medical_cancel: Migration to 13.0

--- cb_medical_cancel/tests/test_cancel_reason.py
+++ cb_medical_cancel/tests/test_cancel_reason.py
@@ -7,7 +7,12 @@ class TestCancelReason(TransactionCase):
         super().setUp()
         self.patient = self.env["medical.patient"].create({"name": "Patient"})
         self.center = self.env["res.partner"].create(
-            {"name": "center", "is_center": True, "is_medical": True}
+            {
+                "name": "center",
+                "is_center": True,
+                "is_medical": True,
+                "encounter_sequence_prefix": "C",
+            }
         )
         self.careplan = self.env["medical.careplan"].create(
             {"patient_id": self.patient.id, "center_id": self.center.id}
@@ -38,3 +43,51 @@ class TestCancelReason(TransactionCase):
         ).run()
         self.careplan.refresh()
         self.assertEqual(self.careplan.state, "cancelled")
+
+    def test_cancel_encounter(self):
+        encounter = self.env["medical.encounter"].create(
+            {
+                "name": "Test Encounter",
+                "patient_id": self.patient.id,
+                "center_id": self.center.id,
+            }
+        )
+        laboratory_request = self.env["medical.laboratory.request"].create(
+            {
+                "patient_id": self.patient.id,
+                "careplan_id": self.careplan.id,
+                "center_id": self.center.id,
+                "encounter_id": encounter.id,
+            }
+        )
+        laboratory_request.flush()
+        self.careplan.encounter_id = encounter.id
+        self.assertEqual(self.careplan.state, "draft")
+        self.pos_config = self.env["pos.config"].create({"name": "PoS config"})
+        session = self.env["pos.session"].create(
+            {"config_id": self.pos_config.id, "user_id": self.env.uid}
+        )
+        wizard = self.env["medical.encounter.cancel"].create(
+            {
+                "encounter_id": encounter.id,
+                "cancel_reason_id": self.reason.id,
+                "cancel_reason": "Test cancel reason",
+                "pos_session_id": session.id,
+            }
+        )
+        wizard.flush()
+        wizard.run()
+        encounter.refresh()
+        self.assertEqual(encounter.state, "finished")
+        self.assertTrue(encounter.cancel_reason_id)
+        self.assertEqual(encounter.cancel_reason_id, self.reason)
+        self.assertEqual(self.careplan.state, "cancelled")
+        self.assertEqual(
+            self.careplan.laboratory_request_ids.state, "cancelled"
+        )
+        with self.assertRaises(ValidationError):
+            encounter.state = "cancelled"
+            encounter.cancel(self.reason, session, "Cancel reason")
+        self.careplan.reactive()
+        self.assertEqual(self.careplan.state, "active")
+        self.assertFalse(self.careplan.cancel_reason_id)
