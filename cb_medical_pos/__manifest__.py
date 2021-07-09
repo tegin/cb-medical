37d7b0c4075b43f9908a8f9e0686360bc1d2d05e
Alba Riera <alba.riera@creublanca.es>
2021-07-09 09:26:19 +0200

    [MIG] cb_medical_pos: Migration to 13.0

--- cb_medical_pos/__manifest__.py
+++ cb_medical_pos/__manifest__.py
@@ -4,15 +4,15 @@
 
 {
     "name": "CB Medical link to PoS",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "author": "Eficent, Creu Blanca",
     "depends": [
         "pos_session_pay_invoice",
-        "pos_close_approval",
         "cb_medical_commission",
-        "account_cash_invoice_inter_company",
+        "pos_inter_company",
     ],
     "data": [
+        "views/pos_payment.xml",
         "data/ir_sequence_data.xml",
         "security/cb_medical_pos_security.xml",
         "wizard/wizard_medical_encounter_close_view.xml",
