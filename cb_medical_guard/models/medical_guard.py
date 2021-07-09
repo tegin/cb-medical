tegin/cb-addons

[ADD] cb_medical_guard

cb_medical_guard/models/medical_guard.py
@@ -1,0 +1,121 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+
+class MedicalGuard(models.Model):
+    _name = 'medical.guard'
+    _inherit = ['medical.abstract', 'mail.thread', 'mail.activity.mixin']
+
+    date = fields.Datetime(
+        required=True,
+        track_visibility='onchange',
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+    )
+    end_date = fields.Datetime(
+        required=True,
+        track_visibility='onchange',
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+    )
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('completed', 'Completed')
+    ], required=True, default='draft', readonly=True)
+    practitioner_id = fields.Many2one(
+        'res.partner',
+        domain=[('is_practitioner', '=', True)],
+        track_visibility='onchange',
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+    )
+    location_id = fields.Many2one(
+        'res.partner',
+        domain=[('is_location', '=', True), ('guard_journal_id', '!=', False)],
+        track_visibility='onchange',
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+    )
+    product_id = fields.Many2one(
+        'product.product',
+        required=True,
+        domain=[('type', '=', 'service')],
+        track_visibility='onchange',
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+    )
+    plan_guard_id = fields.Many2one(
+        'medical.guard.plan',
+        readonly=True,
+    )
+    invoice_line_ids = fields.One2many(
+        'account.invoice.line',
+        inverse_name='guard_id',
+    )
+
+    @api.model
+    def _get_internal_identifier(self, vals):
+        return self.env['ir.sequence'].next_by_code(
+            'medical.guard') or '/'
+
+    def _complete_vals(self):
+        return {'state': 'completed'}
+
+    @api.multi
+    def complete(self):
+        self.ensure_one()
+        if not self.practitioner_id:
+            raise ValidationError(_('Practitioner is required'))
+        self.write(self._complete_vals())
+
+    def _get_invoice_vals(self):
+        journal = self.location_id.guard_journal_id
+        invoice = self.env['account.invoice'].new({
+            'partner_id': self.practitioner_id.id,
+            'type': ('in_invoice' if journal.type == 'purchase' else
+                     'in_refund'),
+            'journal_id': journal.id,
+            'company_id': journal.company_id.id,
+            'state': 'draft',
+        })
+        # Get other invoice values from onchanges
+        invoice._onchange_partner_id()
+        invoice._onchange_journal_id()
+        return invoice._convert_to_write(invoice._cache)
+
+    def _get_invoice_line_vals(self, invoice):
+        invoice_line = self.env['account.invoice.line'].new({
+            'invoice_id': invoice.id,
+            'product_id': self.product_id.id,
+            'quantity': 1,
+            'guard_id': self.id,
+        })
+        # Get other invoice line values from product onchange
+        invoice_line._onchange_product_id()
+        invoice_line_vals = invoice_line._convert_to_write(invoice_line._cache)
+        lang = self.env['res.lang'].search(
+            [('code', '=', invoice.partner_id.lang or
+              self.env.context.get('lang', 'en_US'))])
+        date = fields.Date.from_string(self.date)
+        invoice_line_vals['name'] = _('%s at %s on %s') % (
+            self.product_id.name,
+            self.practitioner_id.name,
+            date.strftime(lang.date_format))
+        return invoice_line_vals
+
+    def get_invoice(self):
+        inv = self.env['account.invoice'].search([
+            ('partner_id', '=', self.practitioner_id.id),
+            ('type', '=', 'in_invoice'),
+            ('state', '=', 'draft'),
+            ('journal_id', '=', self.location_id.guard_journal_id.id)
+        ])
+        if not inv:
+            inv = inv.create(self._get_invoice_vals())
+        return inv
+
+    def make_invoice(self):
+        inv = self.get_invoice()
+        self.env['account.invoice.line'].create(
+            self._get_invoice_line_vals(inv))
+        return inv

cb_medical_guard/models/medical_guard_plan.py
@@ -1,0 +1,123 @@
+from datetime import timedelta, datetime, date
+import pytz
+from odoo import api, models, fields, _
+from odoo.exceptions import UserError
+
+
+def parse_atom(parse, minmax):
+    parse = parse.strip()
+    increment = 1
+    if parse == '*':
+        return set(range(minmax[0], minmax[1] + 1))
+    elif parse.isdigit():
+        # A single number still needs to be returned as a set
+        value = int(parse)
+        if value >= minmax[0] and value <= minmax[1]:
+            return set((value,))
+        else:
+            raise UserError(_("\"%s\" is not within valid range.") % parse)
+    elif '-' in parse or '/' in parse:
+        divide = parse.split('/')
+        subrange = divide[0]
+        if len(divide) == 2:
+            # Example: 1-3/5 or */7 increment should be 5 and 7 respectively
+            increment = int(divide[1])
+
+        if '-' in subrange:
+            # Example: a-b
+            prefix, suffix = [int(n) for n in subrange.split('-')]
+            if prefix < minmax[0] or suffix > minmax[1]:
+                raise UserError(_("\"%s\" is not within valid range.") % parse)
+        elif subrange == '*':
+            # Include all values with the given range
+            prefix, suffix = minmax
+        else:
+            raise UserError(_("Unrecognized symbol \"%s\"") % subrange)
+
+        if prefix < suffix:
+            # Example: 7-10
+            return set(range(prefix, suffix + 1, increment))
+        else:
+            # Example: 12-4/2; (12, 12 + n, ..., 12 + m*n) U (n_0, ..., 4)
+            noskips = list(range(prefix, minmax[1] + 1))
+            noskips += list(range(minmax[0], suffix + 1))
+            return set(noskips[::increment])
+    else:
+        raise UserError(_("Atom \"%s\" not in a recognized format.") % parse)
+
+
+class MedicalGuardPlan(models.Model):
+    _name = 'medical.guard.plan'
+
+    start_time = fields.Float(required=True)
+    end_time = fields.Float(required=True)
+    location_id = fields.Many2one(
+        'res.partner',
+        domain=[('is_location', '=', True), ('guard_journal_id', '!=', False)],
+        required=True,
+    )
+    product_id = fields.Many2one(
+        'product.product',
+        required=True,
+        domain=[('type', '=', 'service')],
+    )
+    active = fields.Boolean(
+        required=True,
+        default=True,
+    )
+    weekday = fields.Char(
+        required=True,
+        default='*'
+    )
+    monthday = fields.Char(
+        required=True,
+        default='*'
+    )
+    month = fields.Char(
+        required=True,
+        default='*'
+    )
+
+    def _check(self, value, expr_vals, minmax):
+        for expr in expr_vals.split(','):
+            if bool(value in parse_atom(expr, minmax)):
+                return True
+        return False
+
+    @api.multi
+    def check_date(self, date_str):
+        date = date_str
+        if isinstance(date_str, str):
+            date = fields.Date.from_string(date_str)
+        if not self._check(date.weekday(), self.weekday, (0, 6)):
+            return False
+        if not self._check(date.month, self.month, (1, 12)):
+            return False
+        if not self._check(date.day, self.monthday, (1, 31)):
+            return False
+        return True
+
+    def _prepare_guard_vals(self, str_date):
+        date_date = str_date
+        if isinstance(str_date, str):
+            date_date = fields.Date.from_string(str_date)
+        datetime_date = date_date
+        tz = pytz.timezone(self.env.context.get('tz') or 'UTC')
+        if isinstance(date_date, date):
+            datetime_date = tz.localize(datetime(
+                date_date.year, date_date.month, date_date.day))
+        return {
+            'date': fields.Datetime.to_string(
+                (datetime_date + timedelta(hours=self.start_time)).astimezone(
+                    tz=None)),
+            'end_date': fields.Datetime.to_string(
+                (datetime_date + timedelta(hours=self.end_time)).astimezone(
+                    tz=None)),
+            'location_id': self.location_id.id,
+            'product_id': self.product_id.id,
+            'plan_guard_id': self.id,
+        }
+
+    @api.multi
+    def apply_plan(self, date):
+        return self.env['medical.guard'].create(self._prepare_guard_vals(date))

tegin/cb-medical

cb_medical_guard/models/medical_guard.py
@@ -1,0 +1,123 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+
+class MedicalGuard(models.Model):
+    _name = 'medical.guard'
+    _inherit = ['medical.abstract', 'mail.thread', 'mail.activity.mixin']
+
+    date = fields.Datetime(
+        required=True,
+        track_visibility='onchange',
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+    )
+    end_date = fields.Datetime(
+        required=True,
+        track_visibility='onchange',
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+    )
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('completed', 'Completed')
+    ], required=True, default='draft', readonly=True)
+    practitioner_id = fields.Many2one(
+        'res.partner',
+        domain=[('is_practitioner', '=', True)],
+        track_visibility='onchange',
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+    )
+    location_id = fields.Many2one(
+        'res.partner',
+        domain=[('is_center', '=', True), ('guard_journal_id', '!=', False)],
+        track_visibility='onchange',
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+    )
+    product_id = fields.Many2one(
+        'product.product',
+        required=True,
+        domain=[('type', '=', 'service')],
+        track_visibility='onchange',
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+    )
+    plan_guard_id = fields.Many2one(
+        'medical.guard.plan',
+        readonly=True,
+    )
+    invoice_line_ids = fields.One2many(
+        'account.invoice.line',
+        inverse_name='guard_id',
+    )
+
+    @api.model
+    def _get_internal_identifier(self, vals):
+        return self.env['ir.sequence'].next_by_code(
+            'medical.guard') or '/'
+
+    def _complete_vals(self):
+        return {'state': 'completed'}
+
+    @api.multi
+    def complete(self):
+        self.ensure_one()
+        if not self.practitioner_id:
+            raise ValidationError(_('Practitioner is required'))
+        self.write(self._complete_vals())
+
+    def _get_invoice_vals(self):
+        journal = self.location_id.guard_journal_id
+        invoice = self.env['account.invoice'].new({
+            'partner_id': self.practitioner_id.id,
+            'type': ('in_invoice' if journal.type == 'purchase' else
+                     'in_refund'),
+            'journal_id': journal.id,
+            'company_id': journal.company_id.id,
+            'state': 'draft',
+        })
+        # Get other invoice values from onchanges
+        invoice._onchange_partner_id()
+        invoice._onchange_journal_id()
+        return invoice._convert_to_write(invoice._cache)
+
+    def _get_invoice_line_vals(self, invoice):
+        invoice_line = self.env['account.invoice.line'].new({
+            'invoice_id': invoice.id,
+            'product_id': self.product_id.id,
+            'quantity': 1,
+            'guard_id': self.id,
+        })
+        # Get other invoice line values from product onchange
+        invoice_line._onchange_product_id()
+        invoice_line_vals = invoice_line._convert_to_write(invoice_line._cache)
+        lang = self.env['res.lang'].search(
+            [('code', '=', invoice.partner_id.lang or
+              self.env.context.get('lang', 'en_US'))])
+        date = fields.Date.from_string(self.date)
+        invoice_line_vals['name'] = _('%s at %s on %s') % (
+            self.product_id.name,
+            self.practitioner_id.name,
+            date.strftime(lang.date_format))
+        return invoice_line_vals
+
+    def get_invoice(self):
+        journal = self.location_id.guard_journal_id
+        inv = self.env['account.invoice'].search([
+            ('partner_id', '=', self.practitioner_id.id),
+            ('type', '=', ('in_invoice' if journal.type == 'purchase' else
+                           'in_refund')),
+            ('state', '=', 'draft'),
+            ('journal_id', '=', journal.id)
+        ])
+        if not inv:
+            inv = inv.create(self._get_invoice_vals())
+        return inv
+
+    def make_invoice(self):
+        inv = self.get_invoice()
+        self.env['account.invoice.line'].create(
+            self._get_invoice_line_vals(inv))
+        return inv

cb_medical_guard/models/medical_guard_plan.py
@@ -1,0 +1,128 @@
+from datetime import timedelta, datetime, date
+import pytz
+from odoo import api, models, fields, _
+from odoo.exceptions import UserError
+
+
+def parse_atom(parse, minmax):
+    parse = parse.strip()
+    increment = 1
+    if parse == '*':
+        return set(range(minmax[0], minmax[1] + 1))
+    elif parse.isdigit():
+        # A single number still needs to be returned as a set
+        value = int(parse)
+        if minmax[0] <= value <= minmax[1]:
+            return set((value,))
+        else:
+            raise UserError(_("\"%s\" is not within valid range.") % parse)
+    elif '-' in parse or '/' in parse:
+        divide = parse.split('/')
+        subrange = divide[0]
+        if len(divide) == 2:
+            # Example: 1-3/5 or */7 increment should be 5 and 7 respectively
+            increment = int(divide[1])
+
+        if '-' in subrange:
+            # Example: a-b
+            prefix, suffix = [int(n) for n in subrange.split('-')]
+            if prefix < minmax[0] or suffix > minmax[1]:
+                raise UserError(_("\"%s\" is not within valid range.") % parse)
+        elif subrange == '*':
+            # Include all values with the given range
+            prefix, suffix = minmax
+        else:
+            raise UserError(_("Unrecognized symbol \"%s\"") % subrange)
+
+        if prefix < suffix:
+            # Example: 7-10
+            return set(range(prefix, suffix + 1, increment))
+        else:
+            # Example: 12-4/2; (12, 12 + n, ..., 12 + m*n) U (n_0, ..., 4)
+            noskips = list(range(prefix, minmax[1] + 1))
+            noskips += list(range(minmax[0], suffix + 1))
+            return set(noskips[::increment])
+    else:
+        raise UserError(_("Atom \"%s\" not in a recognized format.") % parse)
+
+
+class MedicalGuardPlan(models.Model):
+    _name = 'medical.guard.plan'
+
+    start_time = fields.Float(required=True)
+    end_time = fields.Float(required=True)
+    location_id = fields.Many2one(
+        'res.partner',
+        domain=[('is_center', '=', True), ('guard_journal_id', '!=', False)],
+        required=True,
+    )
+    product_id = fields.Many2one(
+        'product.product',
+        required=True,
+        domain=[('type', '=', 'service')],
+    )
+    active = fields.Boolean(
+        required=True,
+        default=True,
+    )
+    weekday = fields.Char(
+        required=True,
+        default='*',
+        help="Use it in order to filter the weekdays, where 0 is monday "
+             "and 6 sunday. You could use 0-4 in order to select Monday to "
+             "Friday. Use * for all.",
+    )
+    monthday = fields.Char(
+        required=True,
+        default='*',
+        help="Use it in order to filter the day of the month. Use * for all.",
+    )
+    month = fields.Char(
+        required=True,
+        default='*',
+        help="Use it in order to filter the month. Use * for all.",
+    )
+
+    def _check(self, value, expr_vals, minmax):
+        for expr in expr_vals.split(','):
+            if bool(value in parse_atom(expr, minmax)):
+                return True
+        return False
+
+    @api.multi
+    def check_date(self, date_str):
+        date = date_str
+        if isinstance(date_str, str):
+            date = fields.Date.from_string(date_str)
+        if not self._check(date.weekday(), self.weekday, (0, 6)):
+            return False
+        if not self._check(date.month, self.month, (1, 12)):
+            return False
+        if not self._check(date.day, self.monthday, (1, 31)):
+            return False
+        return True
+
+    def _prepare_guard_vals(self, str_date):
+        date_date = str_date
+        if isinstance(str_date, str):
+            date_date = fields.Date.from_string(str_date)
+        datetime_date = date_date
+        tz = pytz.timezone(self.env.context.get('tz') or 'UTC')
+        if isinstance(date_date, date):
+            datetime_date = tz.localize(datetime(
+                date_date.year, date_date.month, date_date.day))
+        return {
+            'date': fields.Datetime.to_string(
+                (datetime_date + timedelta(hours=self.start_time)).astimezone(
+                    tz=None)),
+            'end_date': fields.Datetime.to_string(
+                (datetime_date + timedelta(hours=self.end_time)).astimezone(
+                    tz=None)),
+            'location_id': self.location_id.id,
+            'product_id': self.product_id.id,
+            'plan_guard_id': self.id,
+        }
+
+    @api.multi
+    def apply_plan(self, date):
+        return self.env['medical.guard'].create(self._prepare_guard_vals(date))