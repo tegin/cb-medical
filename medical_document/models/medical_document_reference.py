ad2460d3f89b7a4dbfe66d7bdf98cfbf5bf9b219
Enric Tobella <etobella@creublanca.es>
2021-07-10 00:41:19 +0200

    [IMP] medical_document: Improve tests, remove unnecessary code

--- medical_document/models/medical_document_reference.py
+++ medical_document/models/medical_document_reference.py
@@ -60,9 +60,6 @@ class MedicalDocumentReference(models.Model):
     def _get_language(self):
         return self.lang or self.patient_id.lang
 
-    def check_is_billable(self):
-        return self.is_billable
-
     def _get_internal_identifier(self, vals):
         return (
             self.env["ir.sequence"].next_by_code("medical.document.reference")
--- medical_document/tests/__init__.py
+++ medical_document/tests/__init__.py
@@ -1,3 +1,4 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from . import test_document_type
+from . import test_document
