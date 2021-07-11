f2831298222a8f75e93741eb134261eb12bc65ac
Enric Tobella <etobella@creublanca.es>
2021-07-11 09:50:38 +0200

    [IMP] cb_medical_views: Adding tests

--- cb_medical_views/models/__init__.py
+++ cb_medical_views/models/__init__.py
@@ -3,4 +3,3 @@ from . import medical_coverage_template
 from . import medical_encounter
 from . import medical_request
 from . import medical_event
-from . import res_partner
--- cb_medical_views/models/medical_coverage.py
+++ cb_medical_views/models/medical_coverage.py
@@ -4,7 +4,7 @@ from odoo import api, models
 class MedicalCoverage(models.Model):
     _inherit = "medical.coverage"
 
-    @api.depends("subscriber_id", "internal_identifier")
+    @api.depends("subscriber_id", "internal_identifier", "name")
     def name_get(self):
         result = []
         for rec in self:
