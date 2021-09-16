aa65234bae0f3c1c8a699cdebe825b1e2042bd56
Enric Tobella <etobella@creublanca.es>
2021-09-16 09:38:32 +0200

    [FIX] migration scripts

--- cb_medical_invoice/migrations/13.0.1.0.0/post-migration.py
+++ cb_medical_invoice/migrations/13.0.1.0.0/post-migration.py
@@ -9,7 +9,7 @@ def migrate(env, version):
         env.cr,
         """
         UPDATE account_move am
-        SET encounter_final_invoice = ai.encounter_final_invoice,
+        SET encounter_final_invoice = ai.encounter_final_invoice
         FROM account_invoice ai
         WHERE ai.id = am.old_invoice_id""",
     )
