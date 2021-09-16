f624af1149f5f202e9817e422947bb3933badb55
Enric Tobella <etobella@creublanca.es>
2021-09-16 09:38:32 +0200

    [FIX] migration scripts

---
+++ cb_medical_guard/migrations/13.0.1.0.0/post-migration.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE account_move_line aml
+        SET guard_id = ail.guard_id
+        FROM account_invoice_line ail
+        WHERE ail.id = aml.old_invoice_line_id""",
+    )
