3230d21a98f70c362490c54c01c5726b3c1ff921
Enric Tobella <etobella@creublanca.es>
2021-09-16 12:13:44 +0200

    [FIX] cb_medical_pos migration script

--- cb_medical_pos/migrations/13.0.1.0.0/post-migration.py
+++ cb_medical_pos/migrations/13.0.1.0.0/post-migration.py
@@ -16,3 +16,14 @@ def migrate(env, version):
         WHERE ail.id = aml.old_invoice_line_id
             and ail.down_payment_line_id is not null""",
     )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE sale_order_line sol
+        SET down_payment_line_id = aml.id
+        FROM account_move_line aml
+        WHERE aml.old_invoice_line_id = sol.{}
+            AND sol.down_payment_line_id is NOT NULL""".format(
+            openupgrade.get_legacy_name("down_payment_line_id")
+        ),
+    )
