3c20fce40a39cd91de4f931ff0e08944e9ca4168
Enric Tobella <etobella@creublanca.es>
2022-01-26 10:36:37 +0100

    [FIX] cb_medical_medcation: Wrong set of

--- cb_medical_medication/models/product.py
+++ cb_medical_medication/models/product.py
@@ -5,9 +5,20 @@ class ProductProduct(models.Model):
     _inherit = "product.product"
 
     @api.model
-    def _name_search(self, name="", args=None, operator="ilike", limit=100):
+    def _name_search(
+        self,
+        name="",
+        args=None,
+        operator="ilike",
+        limit=100,
+        name_get_uid=None,
+    ):
         res = super()._name_search(
-            name=name, args=args, operator=operator, limit=limit
+            name=name,
+            args=args,
+            operator=operator,
+            limit=limit,
+            name_get_uid=name_get_uid,
         )
         if not res and self.env.context.get("search_on_supplier"):
             suppliers = self.env["product.supplierinfo"].search(
