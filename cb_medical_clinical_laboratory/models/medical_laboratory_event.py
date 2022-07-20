90734e9a03deabc5f3ac9b486ee392dc7d0c5a7f
Enric Tobella <etobella@creublanca.es>
2022-07-20 17:36:10 +0200

    [FIX] cb_medical_clinical_laboratory: fix rebase

--- cb_medical_clinical_laboratory/models/medical_laboratory_event.py
+++ cb_medical_clinical_laboratory/models/medical_laboratory_event.py
@@ -55,59 +55,6 @@ class MedicalLaboratoryEvent(models.Model):
             return False
         return super(MedicalLaboratoryEvent, self)._get_is_billable()
 
-    @api.onchange("laboratory_service_id", "laboratory_request_id")
-    def _onchange_laboratory_service(self):
-        for rec in self:
-            rec.laboratory_code = rec.laboratory_service_id.laboratory_code
-            rec.name = rec.laboratory_service_id.name
-            cov = (
-                rec.laboratory_request_id.careplan_id.coverage_id.coverage_template_id
-            )
-            price = rec.laboratory_service_id.service_price_ids.filtered(
-                lambda r: r.laboratory_code == cov.laboratory_code
-            )
-            cai = self.env["medical.coverage.agreement.item"].get_item(
-                self.service_id, cov, self.laboratory_request_id.center_id
-            )
-            if (
-                rec.laboratory_service_id.id
-                in rec.laboratory_request_id.laboratory_service_ids.ids
-            ):
-                rec.is_sellable_insurance = False
-                rec.is_sellable_private = False
-                rec.coverage_amount = 0
-                rec.coverage_cost = 0
-                rec.private_amount = 0
-                rec.private_cost = 0
-            elif price and cai:
-                rec.coverage_agreement_id = cai.coverage_agreement_id
-                if cai.coverage_percentage > 0:
-                    rec.is_sellable_insurance = True
-                    rec.coverage_amount = (
-                        price.amount * cai.coverage_percentage / 100
-                    )
-                    rec.coverage_cost = (
-                        price.cost * cai.coverage_percentage / 100
-                    )
-                else:
-                    rec.is_sellable_insurance = False
-                    rec.coverage_cost = 0
-                    rec.coverage_amount = 0
-                if cai.coverage_percentage < 100:
-                    rec.is_sellable_private = True
-                    rec.private_amount = (
-                        price.amount * (100 - cai.coverage_percentage) / 100
-                    )
-                    rec.private_cost = (
-                        price.cost * (100 - cai.coverage_percentage) / 100
-                    )
-                else:
-                    rec.is_sellable_private = False
-                    rec.private_amount = 0
-                    rec.private_cost = 0
-            elif rec.laboratory_service_id:
-                raise ValidationError(_("Laboratory service is not covered."))
-
     def _check_request(self):
         requests = (
             self.laboratory_sample_id.encounter_id.laboratory_request_ids
