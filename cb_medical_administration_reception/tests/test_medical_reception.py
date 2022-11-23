713401f6be94838504c7eb254e0b09744c3af6f2
Núria Sancho <nuria.sancho@creublanca.es>
2022-11-23 17:38:44 +0100

    [14.0][MIG] cb_medical_administration_reception: Migration to 14.0

--- cb_medical_administration_reception/tests/test_medical_reception.py
+++ cb_medical_administration_reception/tests/test_medical_reception.py
@@ -10,19 +10,16 @@ class TestMedicalLocation(TransactionCase):
     def test_center(self):
         vals = {
             "name": "reception",
-            "description": "location_description",
             "is_reception": True,
         }
         with self.assertRaises(ValidationError):
             self.env["res.partner"].create(vals)
         center_vals = {
             "name": "test name",
-            "description": "test description",
             "is_center": True,
         }
         center = self.env["res.partner"].create(center_vals)
         self.assertTrue(center.is_center)
-        self.assertTrue(center.center_identifier)
         self.assertEqual(center.reception_count, 0)
         vals["center_id"] = center.id
         location = self.env["res.partner"].create(vals)
