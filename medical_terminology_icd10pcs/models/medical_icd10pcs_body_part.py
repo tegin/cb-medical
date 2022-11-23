e5524a4c5d08c2131f032dfdccbaac6ef0f8253a
Kevin Luna <kevin.luna@creublanca.es>
2022-11-23 12:26:29 +0100

    [IMP] medical_terminology_icd10pcs: black, isort, prettier

--- medical_terminology_icd10pcs/models/medical_icd10pcs_body_part.py
+++ medical_terminology_icd10pcs/models/medical_icd10pcs_body_part.py
@@ -11,9 +11,7 @@ class MedicalIcd10pcsBodyPart(models.Model):
 
     code = fields.Char(required=True)
     name = fields.Char(translate=True)
-    body_system_id = fields.Many2one(
-        "medical.icd10pcs.body.system", required=True
-    )
+    body_system_id = fields.Many2one("medical.icd10pcs.body.system", required=True)
     section_id = fields.Many2one(
         "medical.icd10pcs.section",
         realated="body_system_id.section_id",
