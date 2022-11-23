ca0e8497e6d6fd73efbb61826f95122d9e4f86e5
Núria Sancho <nuria.sancho@creublanca.es>
2022-11-23 09:31:08 +0100

    [14.0][MIG] cb_medical_diagnostic_report: Migration to 14.0

--- cb_medical_diagnostic_report/models/medical_department.py
+++ cb_medical_diagnostic_report/models/medical_department.py
@@ -24,4 +24,4 @@ class MedicalDepartment(models.Model):
     )
 
     def _get_internal_identifier(self, vals):
-        return self.env["ir.sequence"].next_by_code("medical.department") or "/"
+        return self.env["ir.sequence"].sudo().next_by_code("medical.department") or "/"
