908f3143e5dba53ac8595cf8db58aaeacf935e5b
Núria Sancho <nuria.sancho@creublanca.es>
2022-11-23 17:38:44 +0100

    [14.0][MIG] cb_medical_administration_reception: Migration to 14.0

--- cb_medical_administration_reception/__manifest__.py
+++ cb_medical_administration_reception/__manifest__.py
@@ -4,15 +4,15 @@
 
 {
     "name": "Medical Administration Location",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Medical",
     "website": "https://github.com/tegin/cb-medical",
-    "author": "Creu Blanca, Eficent",
+    "author": "CreuBlanca, Eficent",
     "license": "AGPL-3",
     "depends": ["medical_administration_center"],
     "data": [
-        "security/medical_security.xml",
-        "data/ir_sequence_data.xml",
+        # "security/medical_security.xml",
+        # "data/ir_sequence_data.xml",
         "views/res_partner_views.xml",
     ],
     "installable": True,
