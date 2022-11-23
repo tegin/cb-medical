246d54c76fa44ebfa8be625091923ebb5b747bcb
Núria Sancho <nuria.sancho@creublanca.es>
2022-11-23 13:10:19 +0100

    [14.0][MIG] cb_medical_workflow_activity: Migration to 14.0

--- setup/cb_medical_workflow_activity/setup.py
+++ setup/cb_medical_workflow_activity/setup.py
@@ -2,5 +2,10 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
-    odoo_addon=True,
+    odoo_addon={
+    "depends_override": {
+            "remote_report_to_printer_label": "git+https://github.com/tegin/cb-addons.git@14.0#subdirectory=setup/remote_report_to_printer_label",
+            "medical_clinical_procedure": "git+https://github.com/tegin/medical-fhir.git@14.0#subdirectory=setup/medical_clinical_procedure"
+        }
+    },
 )
