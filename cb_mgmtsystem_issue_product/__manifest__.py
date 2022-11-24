67467d894aff50a9475ae55aba946b500cdf2744
Kevin Luna <kevin.luna@creublanca.es>
2022-11-24 15:41:02 +0100

    [MIG] cb_mgmtsystem_issue_product: migration 14.0

--- cb_mgmtsystem_issue_product/__manifest__.py
+++ cb_mgmtsystem_issue_product/__manifest__.py
@@ -5,9 +5,9 @@
     "name": "Cb Mgmtsystem Issue Product",
     "summary": """
         Enables to relate quality issues with products""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "author": "CreuBlanca,Odoo Community Association (OCA)",
     "website": "https://github.com/tegin/cb-medical",
     "depends": ["cb_mgmtsystem_issue", "product"],
     "data": ["views/product_template.xml"],
