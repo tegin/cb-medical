161300499bb5f8f432f6a87ffc44f13765c33e2d
Núria Sancho <nuria.sancho@creublanca.es>
2022-11-24 16:26:05 +0100

    [14.0][MIG] cb_nonconformity_encounter: Migration to 14.0

--- setup/cb_nonconformity_encounter/setup.py
+++ setup/cb_nonconformity_encounter/setup.py
@@ -2,5 +2,10 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
-    odoo_addon=True,
+    odoo_addon={
+    "depends_override": {
+            "cb_mgmtsystem_issue": "git+https://github.com/tegin/cb-addons.git@14.0#subdirectory=setup/cb_mgmtsystem_issue",
+             "medical_administration_encounter": "git+https://github.com/tegin/medical-fhir.git@14.0#subdirectory=setup/medical_administration_encounter"
+        }
+    },
 )
