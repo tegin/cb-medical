e25c8cc59a9d447a65a88e8c9def1b1daef8a726
Kevin Luna <kevin.luna@creublanca.es>
2022-11-28 12:45:49 +0100

    [IMP] medical_authorization: black, isort, prettier

--- medical_authorization/models/medical_coverage_agreement_item.py
+++ medical_authorization/models/medical_coverage_agreement_item.py
@@ -7,15 +7,8 @@ class MedicalCoverageAgreementItem(models.Model):
     def _check_authorization(self, method, **kwargs):
         res = super()._check_authorization(method, **kwargs)
         res.update(
-            {
-                "authorization_checked": kwargs.get(
-                    "authorization_checked", False
-                )
-            }
+            {"authorization_checked": kwargs.get("authorization_checked", False)}
         )
-        if (
-            not kwargs.get("authorization_checked", False)
-            and method.check_required
-        ):
+        if not kwargs.get("authorization_checked", False) and method.check_required:
             res["authorization_status"] = "pending"
         return res
