2fa372d5fcb755da186e981d4809e32a75ae79fa
Kevin Luna <kevin.luna@creublanca.es>
2022-12-01 13:01:03 +0100

    [MIG ] crm_agreement: migration to 14.0

--- crm_agreement/__manifest__.py
+++ crm_agreement/__manifest__.py
@@ -5,9 +5,9 @@
     "name": "Crm Agreement",
     "summary": """
         Link of Medical Agreements and CRM""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "author": "CreuBlanca,Odoo Community Association (OCA)",
     "website": "https://github.com/tegin/cb-medical",
     "depends": [
         "sale_crm",
@@ -16,6 +16,7 @@
         "cb_medical_administration_requester",
     ],
     "data": [
+        "security/ir.model.access.csv",
         "data/mail_data.xml",
         "views/medical_quote.xml",
         "wizards/crm_lead_add_agreement.xml",
