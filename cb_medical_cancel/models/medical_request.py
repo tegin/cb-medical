c87bafc5ab180052e0051fc76b8b02fa61d8f5ef
Kevin Luna <kevin.luna@creublanca.es>
2022-12-05 17:34:28 +0100

    [IMP] cb_medical_cancel: black, isort, prettier

--- cb_medical_cancel/models/medical_request.py
+++ cb_medical_cancel/models/medical_request.py
@@ -5,43 +5,30 @@ from odoo.exceptions import ValidationError
 class MedicalRequest(models.AbstractModel):
     _inherit = "medical.request"
 
-    cancel_reason_id = fields.Many2one(
-        "medical.cancel.reason", string="Cancel reason"
-    )
+    cancel_reason_id = fields.Many2one("medical.cancel.reason", string="Cancel reason")
 
     @api.constrains("state", "cancel_reason_id")
     def check_cancel_reason_state(self):
         for rec in self:
             if rec.state == "cancelled" and not rec.cancel_reason_id:
                 raise ValidationError(
-                    _(
-                        "A cancellation reason is required for cancelled requests"
-                    )
+                    _("A cancellation reason is required for cancelled requests")
                 )
             if rec.state != "cancelled" and rec.cancel_reason_id:
                 raise ValidationError(
-                    _(
-                        "A cancellation reason is only allowed on cancelled "
-                        "requests"
-                    )
+                    _("A cancellation reason is only allowed on cancelled " "requests")
                 )
 
     def cancel_values(self):
         vals = super().cancel_values()
         vals.update(
-            {
-                "cancel_reason_id": self.env.context.get(
-                    "cancel_reason_id", False
-                )
-            }
+            {"cancel_reason_id": self.env.context.get("cancel_reason_id", False)}
         )
         return vals
 
     def _check_cancellable(self):
         return not self.filtered(
-            lambda r: (
-                r.state in ["completed", "entered-in-error", "cancelled"]
-            )
+            lambda r: (r.state in ["completed", "entered-in-error", "cancelled"])
             and (
                 not r.authorization_method_id.check_required
                 or r.authorization_status != "authorized"
