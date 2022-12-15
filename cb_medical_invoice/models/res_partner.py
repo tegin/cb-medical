c62a084baffd05e309f07f46757e33c0173fcd34
Núria Sancho <nuria.sancho@creublanca.es>
2022-12-15 16:19:13 +0100

    [14.0][MIG] 14.0-mig-cb_medical_invoice : Migration to 14.0
    Co-authored-by: Enric Tobella <etobella@creublanca.es>

--- cb_medical_invoice/models/res_partner.py
+++ cb_medical_invoice/models/res_partner.py
@@ -10,37 +10,4 @@ class ResPartner(models.Model):
         "partner_id",
         "patient_id",
     )
-
-    self_invoice_refund_sequence_id = fields.Many2one(
-        comodel_name="ir.sequence",
-        string="Self invoice refund sequence",
-        ondelete="restrict",
-    )
-
-    def set_self_invoice(self):
-        super().set_self_invoice()
-        for record in self:
-            if record.self_invoice_refund_sequence_id:
-                continue
-            if record.self_invoice:
-                record.self_invoice_refund_sequence_id = (
-                    self.env["ir.sequence"]
-                    .sudo()
-                    .create(
-                        {
-                            "name": record.name + " Self invoice refund sequence",
-                            "implementation": "no_gap",
-                            "number_increment": 1,
-                            "padding": 4,
-                            "prefix": self._self_invoice_refund_sequence_prefix(),
-                            "use_date_range": True,
-                            "number_next": 1,
-                        }
-                    )
-                )
-
-    def _self_invoice_sequence_prefix(self):
-        return "CBINV/%(range_year)s/"
-
-    def _self_invoice_refund_sequence_prefix(self):
-        return "CBINV/R/%(range_year)s/"
+    self_invoice = fields.Boolean(company_dependent=False)
