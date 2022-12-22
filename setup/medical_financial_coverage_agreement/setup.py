c0ff0540dc8de931ae68683c0977ea2183f2c8fd
Enric Tobella <etobella@creublanca.es>
2022-12-22 15:33:50 +0100

    [PORT] medical_administration_center to medical-fhir

--- setup/medical_financial_coverage_agreement/setup.py
+++ setup/medical_financial_coverage_agreement/setup.py
@@ -7,6 +7,7 @@ setuptools.setup(
             "medical_financial_coverage": "git+https://github.com/tegin/medical-fhir.git@14.0#subdirectory=setup/medical_financial_coverage",
             "medical_workflow": "git+https://github.com/tegin/medical-fhir.git@14.0#subdirectory=setup/medical_workflow",
             "product_nomenclature": "git+https://github.com/tegin/cb-addons.git@14.0#subdirectory=setup/product_nomenclature",
+            "medical_administration_center": "git+https://github.com/tegin/medical-fhir.git@14.0#subdirectory=setup/medical_administration_center",
         }
     },
 )
