d551d2c468f438da23e686a40d6cca354761ccd8
Enric Tobella <etobella@creublanca.es>
2023-06-29 12:21:22 +0200

    [IMP] medical_turn: Add constrains and domain

--- medical_turn_management/models/medical_turn.py
+++ medical_turn_management/models/medical_turn.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class MedicalTurn(models.Model):
@@ -20,6 +21,7 @@ class MedicalTurn(models.Model):
     practitioner_id = fields.Many2one(
         string="Practitioner",
         comodel_name="res.partner",
+        domain=[("is_practitioner", "=", True)],
         tracking=True,
     )
     specialty_id = fields.Many2one(
@@ -36,6 +38,25 @@ class MedicalTurn(models.Model):
         default=lambda r: fields.Datetime.now(),
     )
     duration = fields.Float("Duration (in hours)", tracking=True, required=True)
+
+    @api.constrains("practitioner_id")
+    def _check_practitioner(self):
+        for record in self:
+            if not record.practitioner_id:
+                continue
+            if not record.practitioner_id.is_practitioner:
+                raise ValidationError(
+                    _("Partner %s must be practitioner")
+                    % record.practitioner_id.display_name
+                )
+            if record.specialty_id not in record.practitioner_id.turn_specialty_ids:
+                raise ValidationError(
+                    _("Specialty %s is not done by %s")
+                    % (
+                        record.specialty_id.display_name,
+                        record.practitioner_id.display_name,
+                    )
+                )
 
     @api.depends("practitioner_id", "center_ids", "specialty_id")
     def _compute_display_name(self):
