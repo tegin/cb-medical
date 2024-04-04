d46dac97997912be980212d6b4f5b998de9a2842
Enric Tobella <enric.tobella@dixmit.com>
2024-04-04 23:51:51 +0200

    [IMP] medical_queue_management:Add token list

--- medical_queue_management/static/src/js/clipboardListField.js
+++ medical_queue_management/static/src/js/clipboardListField.js
@@ -9,10 +9,16 @@ odoo.define("medical_queue_management.CopyClipboardListChar", function (require)
         events: {
             "click .o_clipboard_list_view": "_onClipboardClick",
         },
+        getValue: function () {
+            if (typeof this.value === "object") {
+                return this.value.data.display_name;
+            }
+            return this.value;
+        },
         _onClipboardClick: function (ev) {
             ev.preventDefault();
             ev.stopPropagation();
-            navigator.clipboard.writeText(this.value);
+            navigator.clipboard.writeText(this.getValue());
         },
     });
 
