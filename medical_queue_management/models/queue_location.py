236bf7105fc7e68ca7f7b064c93b3c33f8e89d26
Kevin Luna <kevin.luna@creublanca.es>
2024-12-05 16:46:07 +0100

    [IMP] medical_queue_management: add busy state in display name wizard

--- medical_queue_management/models/queue_location.py
+++ medical_queue_management/models/queue_location.py
@@ -1,7 +1,7 @@
 # Copyright 2024 Dixmit
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import _, fields, models
 
 
 class QueueLocation(models.Model):
@@ -10,3 +10,16 @@ class QueueLocation(models.Model):
 
     action_ids = fields.Many2many("queue.location.action")
     allows_flag = fields.Boolean()
+
+    def name_get(self):
+        previous_result = super().name_get()
+        if not self.env.context.get("custom_search"):
+            return previous_result
+        res = []
+        for record, (record_id, base_name) in zip(self, previous_result):
+            if record.state == "working":
+                display_name = f"{base_name} ({_('Busy')} 🔴)"
+            else:
+                display_name = f"{base_name} 🟢"
+            res.append((record_id, display_name))
+        return res
