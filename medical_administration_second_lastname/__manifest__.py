5a9c9580b0d7d30bb6040001adb012321c393927
Kevin Luna <kevin.luna@creublanca.es>
2025-01-15 12:10:40 +0100

    [MIG] medical_administration_second_lastname: migration to 16.0

--- medical_administration_second_lastname/__manifest__.py
+++ medical_administration_second_lastname/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Medical Patient second lastname",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Eficent, CreuBlanca",
     "depends": ["medical_administration_firstname", "partner_second_lastname"],
     "data": ["views/medical_patient_views.xml"],
