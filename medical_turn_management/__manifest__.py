803bfbe4f1c240668e6bfb1c21bf7df81db4f884
Kevin Luna <kevin.luna@creublanca.es>
2025-01-15 16:17:38 +0100

    [IMP] medical_turn_management: black, isort, prettier

--- medical_turn_management/__manifest__.py
+++ medical_turn_management/__manifest__.py
@@ -5,13 +5,13 @@
     "name": "Medical Turn Management",
     "summary": """
         Manage Profesional turn management""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "CreuBlanca,Odoo Community Association (OCA)",
     "website": "https://github.com/tegin/cb-medical",
     "depends": [
-        "medical_administration_practitioner",
-        "web_view_calendar_list",
+        "medical_administration_practitioner_specialty",
+        # "web_view_calendar_list",
         "medical_base",
         "medical_administration_center",
         "mail",
