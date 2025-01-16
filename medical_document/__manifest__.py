d871b55746f1a7ac2f7ebd36c4bb610e9ef6c4e1
Enric Tobella <enric.tobella@dixmit.com>
2025-01-16 10:31:48 +0100

    [MIG] medical_document: Use qweb + migration scripts

--- medical_document/__manifest__.py
+++ medical_document/__manifest__.py
@@ -26,7 +26,4 @@
     "license": "AGPL-3",
     "installable": True,
     "auto_install": False,
-    "external_dependencies": {
-        "python": ["mock"],
-    },
 }
--- medical_document/tests/test_document.py
+++ medical_document/tests/test_document.py
@@ -1,4 +1,4 @@
-from mock import patch
+from unittest.mock import patch
 
 from odoo.exceptions import ValidationError
 from odoo.tests.common import Form, SavepointCase
@@ -47,7 +47,7 @@ class TestDocument(SavepointCase):
                         0,
                         {
                             "lang": cls.lang_en.code,
-                            "text": "<p>%s</p><p>{{object.patient_id.name}}"
+                            "text": "<p>%s</p><p><t t-out='object.patient_id.name'/>"
                             "</p>" % cls.lang_en.code,
                         },
                     ),
@@ -56,7 +56,7 @@ class TestDocument(SavepointCase):
                         0,
                         {
                             "lang": cls.lang_es.code,
-                            "text": "<p>%s</p><p>{{object.patient_id.name}}"
+                            "text": "<p>%s</p><p><t t-out='object.patient_id.name'/>"
                             "</p>" % cls.lang_es.code,
                         },
                     ),
