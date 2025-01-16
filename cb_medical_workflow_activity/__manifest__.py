e0e0503f0122c45477c243cf9ed4f80e9c96eca1
Kevin Luna <kevin.luna@creublanca.es>
2025-01-16 15:45:53 +0100

    [MIG] cb_medical_workflow_activity: migration to 16.0

--- cb_medical_workflow_activity/__manifest__.py
+++ cb_medical_workflow_activity/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "CB Medical Workflow Activity",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Eficent, CreuBlanca",
     "depends": ["medical_clinical_procedure"],
     "data": ["views/product_views.xml"],
