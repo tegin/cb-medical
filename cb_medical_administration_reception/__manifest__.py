ba37c1927ed6878c221e535c37a603965a84a719
Kevin Luna <kevin.luna@creublanca.es>
2025-01-21 09:39:55 +0100

    [MIG] cb_medical_administration_reception: migration to 16.0

--- cb_medical_administration_reception/__manifest__.py
+++ cb_medical_administration_reception/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Medical Administration Location",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Medical",
     "website": "https://github.com/tegin/cb-medical",
     "author": "CreuBlanca, Eficent",
