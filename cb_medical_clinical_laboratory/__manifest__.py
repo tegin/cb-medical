4d97ba1f299152814b93c2c393113fc081affa73
Luis Rodriguez <luis.rodriguez@dixmit.com>
2025-01-28 10:18:09 +0100

    [MIG] cb_medical_clinical_laboratory: Migration to 16.0

--- cb_medical_clinical_laboratory/__manifest__.py
+++ cb_medical_clinical_laboratory/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Medical Clinical Laboratory",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Eficent, CreuBlanca",
     "depends": ["cb_medical_medication"],
     "data": [
