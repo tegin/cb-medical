931ae136cb7e3f187719a01742d7f48e0436d3a4
Enric Tobella <enric.tobella@dixmit.com>
2025-02-12 08:15:39 +0100

    [IMP] cb_medical_commission: Add test enable checks

--- cb_medical_commission/tests/test_commission.py
+++ cb_medical_commission/tests/test_commission.py
@@ -492,7 +492,9 @@ class TestCBMedicalCommission(common.MedicalSavePointCase):
         )
         self.assertTrue(settlements)
         for encounter in encounters:
-            for request in encounter.careplan_ids.mapped("procedure_request_ids"):
+            for request in encounter.careplan_ids.mapped(
+                "procedure_request_ids"
+            ).with_context(test_settle_integrity=True):
                 procedure = request.procedure_ids
                 self.assertEqual(len(procedure.sale_agent_ids), 1)
                 self.assertEqual(len(procedure.invoice_agent_ids), 1)
