67d323bb3af922697bd690fb66a8e68ebb6d6bde
Enric Tobella <enric.tobella@dixmit.com>
2025-03-11 16:55:36 +0100

    [FIX] cb_medical_careplan_sale: Fixing tests

--- cb_medical_careplan_sale/tests/common.py
+++ cb_medical_careplan_sale/tests/common.py
@@ -8,6 +8,16 @@ class MedicalSavePointCase(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        if not cls.env.company.chart_template_id:
+            # Load a CoA if there's none in current company
+            coa = cls.env.ref("l10n_generic_coa.configurable_chart_template", False)
+            if not coa:
+                # Load the first available CoA
+                coa = cls.env["account.chart.template"].search(
+                    [("visible", "=", True)], limit=1
+                )
+            coa.try_loading(company=cls.env.company, install_demo=False)
+
         self = cls
         name = "testing_remote_server"
         self.remote = self.env["res.remote"].search([("name", "=", name)])
