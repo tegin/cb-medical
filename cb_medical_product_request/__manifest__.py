7d5d21a777e9b30d32346b392e1a7ffdef677d7a
Kevin Luna <kevin.luna@creublanca.es>
2025-05-27 12:45:26 +0200

    [MIG] cb_medical_product_reuest: migration to 16.0

--- cb_medical_product_request/__manifest__.py
+++ cb_medical_product_request/__manifest__.py
@@ -6,9 +6,9 @@
     "summary": """
         This addon expands the medical_product_request fhir
         definition adapting it to the spanish prescription system""",
-    "version": "13.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "author": "CreuBlanca,Odoo Community Association (OCA)",
     "website": "https://github.com/tegin/cb-medical",
     "depends": [
         "medical_product_request",
@@ -25,10 +25,14 @@
         "views/medical_product_request.xml",
         "views/medical_product_request_order.xml",
         "views/res_config_settings_views.xml",
-        "templates/assets.xml",
         "data/data.xml",
         "reports/spanish_prescription.xml",
         "reports/spanish_prescription_template.xml",
     ],
+    "assets": {
+        "web.report_assets_common": [
+            "/cb_medical_product_request/static/src/scss/spanish_prescription.scss",
+        ],
+    },
     "demo": ["demo/medical_product_request_demo.xml"],
 }
