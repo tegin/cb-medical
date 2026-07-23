7ce59fa639502124ba9257e0b6a7997dcd5ac012
Enric Tobella <enric.tobella@dixmit.com>
2026-07-23 11:49:36 +0200

    [IMP] medical_financial_coverage_request: remove unnecessary fields and make a better field detection

--- medical_financial_coverage_request/tests/test_encounter_creation.py
+++ medical_financial_coverage_request/tests/test_encounter_creation.py
@@ -1,5 +1,7 @@
+import datetime
 from unittest.mock import patch
 
+from odoo import sql_db
 from odoo.exceptions import ValidationError
 from odoo.tests.common import TransactionCase
 
@@ -15,7 +17,10 @@ class TestEncounterCreate(TransactionCase):
                 "encounter_sequence_prefix": "1",
             }
         )
-        cls.patient = cls.env["medical.patient"].create({"name": "Demo Patient"})
+        with patch.object(
+            sql_db.Cursor, "now", return_value=datetime.datetime(2023, 1, 1, 0, 0, 0)
+        ):
+            cls.patient = cls.env["medical.patient"].create({"name": "Demo Patient"})
 
     def test_create_encounter_constrain_01(self):
         with self.assertRaises(ValidationError):
@@ -67,6 +72,7 @@ class TestEncounterCreate(TransactionCase):
         self.assertEqual(encounter.patient_id.name, "New patient")
 
     def test_create_encounter_write_patient(self):
+        date = self.patient.write_date
         encounter_action = self.env["medical.encounter"].create_encounter(
             patient_vals={"name": "New patient"},
             patient=self.patient,
@@ -78,6 +84,21 @@ class TestEncounterCreate(TransactionCase):
         self.assertEqual(self.center, encounter.center_id)
         self.assertEqual(self.patient, encounter.patient_id)
         self.assertEqual(self.patient.name, "New patient")
+        self.assertNotEqual(self.patient.write_date, date)
+
+    def test_create_encounter_write_patient_unnecessary_fields(self):
+        date = self.patient.write_date
+        encounter_action = self.env["medical.encounter"].create_encounter(
+            patient_vals={"display_name": False, "zip": ""},
+            patient=self.patient,
+            center=self.center,
+        )
+        encounter = self.env[encounter_action["res_model"]].browse(
+            encounter_action["res_id"]
+        )
+        self.assertEqual(self.center, encounter.center_id)
+        self.assertEqual(self.patient, encounter.patient_id)
+        self.assertEqual(self.patient.write_date, date)
 
     def test_create_encounter_write_patient_assert(self):
         with patch.object(type(self.env["medical.patient"]), "write") as patient_write:
